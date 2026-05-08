31ef55b0fb4d8d0e63320914bb240b8cec1f4f0d
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-08 17:51:19 +0530

    add logic for drag-end for single column

--- components/board.tsx
+++ components/board.tsx
@@ -11,7 +11,33 @@ import Column from '@/components/column'
 export default function Board() {
   const [board, setBoard] = useState(initialData)
 
-  function handleDragEnd(result: DropResult<string>) {}
+  function handleDragEnd(result: DropResult<string>) {
+    const { draggableId, source, destination } = result
+
+    if (
+      !destination ||
+      (source.droppableId === destination.droppableId &&
+        source.index === destination.index)
+    ) {
+      return
+    }
+
+    setBoard((board) => {
+      const column = board.columns[source.droppableId]
+      const newTaskIds = [...column.taskIds]
+      newTaskIds.splice(source.index, 1)
+      newTaskIds.splice(destination.index, 0, draggableId)
+
+      const newColumn = { ...column, taskIds: newTaskIds }
+
+      const newBoard = {
+        ...board,
+        columns: { ...board.columns, [newColumn.id]: newColumn },
+      }
+
+      return newBoard
+    })
+  }
 
   return (
     <div>
