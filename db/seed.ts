33e6a53230173ed93da095fe404cb9acfda7d712
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-09 20:23:44 +0530

    update seed to handle fractional indexing

--- db/seed.ts
+++ db/seed.ts
@@ -1,21 +1,40 @@
 import { db } from '@/db'
-import { columnTable, NewTask, taskTable } from '@/db/schema'
+import { columnTable, NewColumn, NewTask, taskTable } from '@/db/schema'
 
 async function main() {
   try {
-    const [{ id: columnId }] = await db
+    const dummyColumns: NewColumn[] = [
+      { title: 'To Do', position: 'a0' },
+      { title: 'In Progress', position: 'a1' },
+      { title: 'Done', position: 'a2' },
+    ]
+
+    const insertedColumns = await db
       .insert(columnTable)
-      .values({
-        title: 'To Do',
-        position: 1000,
-      })
+      .values(dummyColumns)
       .returning()
 
     const dummyTasks: NewTask[] = [
-      { content: 'Take out the garbage', position: 1000, columnId },
-      { content: 'Watch my favorite show', position: 2000, columnId },
-      { content: 'Charge my phone', position: 3000, columnId },
-      { content: 'Cook dinner', position: 4000, columnId },
+      {
+        content: 'Take out the garbage',
+        columnId: insertedColumns[0].id,
+        position: 'a0',
+      },
+      {
+        content: 'Watch my favorite show',
+        columnId: insertedColumns[0].id,
+        position: 'a1',
+      },
+      {
+        content: 'Charge my phone',
+        columnId: insertedColumns[0].id,
+        position: 'a2',
+      },
+      {
+        content: 'Cook dinner',
+        columnId: insertedColumns[0].id,
+        position: 'a3',
+      },
     ]
 
     await db.insert(taskTable).values(dummyTasks)
