e159a23679db427e1ed0a3018cc0ab6857b0cdc7
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-09 09:26:36 +0530

    fix store to be more optimized for the updated state shape

--- lib/store.ts
+++ lib/store.ts
@@ -6,38 +6,33 @@ import { initialData } from '@/lib/data'
 type BoardState = {
   tasks: Record<string, TaskType>
   columns: Record<string, ColumnType>
+  taskOrderByColumn: Record<string, string[]>
   columnOrder: string[]
 
   reorderColumns: ({
-    columnId,
     sourceIndex,
     destinationIndex,
   }: {
-    columnId: string
     sourceIndex: number
     destinationIndex: number
   }) => void
 
   reorderTasks: ({
-    taskId,
     columnId,
     sourceIndex,
     destinationIndex,
   }: {
-    taskId: string
     columnId: string
     sourceIndex: number
     destinationIndex: number
   }) => void
 
   moveTask: ({
-    taskId,
     sourceColumnId,
     destinationColumnId,
     sourceIndex,
     destinationIndex,
   }: {
-    taskId: string
     sourceColumnId: string
     destinationColumnId: string
     sourceIndex: number
@@ -48,54 +43,47 @@ type BoardState = {
 export const useBoard = create<BoardState>()((set) => ({
   ...initialData,
 
-  reorderColumns: ({ columnId, sourceIndex, destinationIndex }) =>
+  reorderColumns: ({ sourceIndex, destinationIndex }) =>
     set(({ columnOrder }) => {
       const newColumnOrder = [...columnOrder]
-      newColumnOrder.splice(sourceIndex, 1)
-      newColumnOrder.splice(destinationIndex, 0, columnId)
+      const [movedColumnId] = newColumnOrder.splice(sourceIndex, 1)
+      newColumnOrder.splice(destinationIndex, 0, movedColumnId)
 
       return { columnOrder: newColumnOrder }
     }),
 
-  reorderTasks: ({ taskId, columnId, sourceIndex, destinationIndex }) =>
-    set(({ columns }) => {
-      const col = columns[columnId]
-      const newTaskIds = [...col.taskIds]
-      newTaskIds.splice(sourceIndex, 1)
-      newTaskIds.splice(destinationIndex, 0, taskId)
+  reorderTasks: ({ columnId, sourceIndex, destinationIndex }) =>
+    set(({ taskOrderByColumn }) => {
+      const newTaskIds = [...taskOrderByColumn[columnId]]
+      const [movedTaskId] = newTaskIds.splice(sourceIndex, 1)
+      newTaskIds.splice(destinationIndex, 0, movedTaskId)
 
-      const newCol = { ...col, taskIds: newTaskIds }
-
-      return { columns: { ...columns, [newCol.id]: newCol } }
+      return {
+        taskOrderByColumn: { ...taskOrderByColumn, [columnId]: newTaskIds },
+      }
     }),
 
   moveTask: ({
-    taskId,
     sourceColumnId,
     destinationColumnId,
     sourceIndex,
     destinationIndex,
   }) =>
-    set(({ columns }) => {
-      const sourceColumn = columns[sourceColumnId]
-      const destinationColumn = columns[destinationColumnId]
+    set(({ taskOrderByColumn }) => {
+      // const sourceColumn = columns[sourceColumnId]
+      // const destinationColumn = columns[destinationColumnId]
 
-      const sourceTaskIds = [...sourceColumn.taskIds]
-      sourceTaskIds.splice(sourceIndex, 1)
-      const newSourceColumn = { ...sourceColumn, taskIds: sourceTaskIds }
+      const sourceTaskIds = [...taskOrderByColumn[sourceColumnId]]
+      const [movedTaskId] = sourceTaskIds.splice(sourceIndex, 1)
 
-      const destinationTaskIds = [...destinationColumn.taskIds]
-      destinationTaskIds.splice(destinationIndex, 0, taskId)
-      const newDestinationColumn = {
-        ...destinationColumn,
-        taskIds: destinationTaskIds,
-      }
+      const destinationTaskIds = [...taskOrderByColumn[destinationColumnId]]
+      destinationTaskIds.splice(destinationIndex, 0, movedTaskId)
 
       return {
-        columns: {
-          ...columns,
-          [sourceColumn.id]: newSourceColumn,
-          [destinationColumn.id]: newDestinationColumn,
+        taskOrderByColumn: {
+          ...taskOrderByColumn,
+          [sourceColumnId]: sourceTaskIds,
+          [destinationColumnId]: destinationTaskIds,
         },
       }
     }),
