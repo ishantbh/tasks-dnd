ddab5cb0ba3d15dde0fec07e84d599a8cef3ba4b
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-09 18:08:40 +0530

    update handleDragEnd logic to update db on drag

--- hooks/useBoardDrag.tsx
+++ hooks/useBoardDrag.tsx
@@ -1,17 +1,26 @@
 import { useShallow } from 'zustand/shallow'
-import type { DropResult } from '@hello-pangea/dnd'
+import { DropResult } from '@hello-pangea/dnd'
 
 import { useBoardStore } from '@/hooks/useBoardStore'
+import { updateColumnPosition, updateTaskPosition } from '@/lib/actions'
+import { getPosition } from '@/lib/utils'
 
 export function useBoardDrag() {
-  const { reorderColumns, reorderTasks, moveTask } = useBoardStore(
+  const {
+    tasks,
+    columns,
+    columnOrder,
+    taskOrderByColumn,
+    setColumnOrder,
+    setTaskOrderByColumn,
+  } = useBoardStore(
     useShallow((state) => ({
+      tasks: state.tasks,
       columns: state.columns,
       columnOrder: state.columnOrder,
+      taskOrderByColumn: state.taskOrderByColumn,
+      setTaskOrderByColumn: state.setTaskOrderByColumn,
       setColumnOrder: state.setColumnOrder,
-      reorderColumns: state.reorderColumns,
-      reorderTasks: state.reorderTasks,
-      moveTask: state.moveTask,
     })),
   )
 
@@ -27,31 +36,62 @@ export function useBoardDrag() {
     }
 
     if (type === 'column') {
-      reorderColumns({
-        sourceIndex: source.index,
-        destinationIndex: destination.index,
-      })
+      const newColumnOrder = [...columnOrder]
+      const [movedColumnId] = newColumnOrder.splice(source.index, 1)
+      newColumnOrder.splice(destination.index, 0, movedColumnId)
+
+      setColumnOrder(newColumnOrder)
+
+      const prev = columns[newColumnOrder[destination.index - 1]]?.position
+      const next = columns[newColumnOrder[destination.index + 1]]?.position
+      const newPosition = getPosition(prev, next)
+
+      await updateColumnPosition({ id: movedColumnId, position: newPosition })
 
       return
     }
 
     // Reorder items in the same column
     if (destination.droppableId === source.droppableId) {
-      reorderTasks({
-        columnId: source.droppableId,
-        sourceIndex: source.index,
-        destinationIndex: destination.index,
+      const newTaskIds = [...taskOrderByColumn[source.droppableId]]
+      const [movedTaskId] = newTaskIds.splice(source.index, 1)
+      newTaskIds.splice(destination.index, 0, movedTaskId)
+
+      setTaskOrderByColumn({
+        ...taskOrderByColumn,
+        [source.droppableId]: newTaskIds,
       })
+
+      const prev = tasks[newTaskIds[destination.index - 1]]?.position
+      const next = tasks[newTaskIds[destination.index + 1]]?.position
+      const newPosition = getPosition(prev, next)
+
+      await updateTaskPosition({ id: movedTaskId, position: newPosition })
 
       return
     }
 
     // Moving items between columns
-    moveTask({
-      sourceColumnId: source.droppableId,
-      destinationColumnId: destination.droppableId,
-      sourceIndex: source.index,
-      destinationIndex: destination.index,
+    const sourceTaskIds = [...taskOrderByColumn[source.droppableId]]
+    const [movedTaskId] = sourceTaskIds.splice(source.index, 1)
+
+    const destinationTaskIds = [...taskOrderByColumn[destination.droppableId]]
+    destinationTaskIds.splice(destination.index, 0, movedTaskId)
+
+    setTaskOrderByColumn({
+      ...taskOrderByColumn,
+      [source.droppableId]: sourceTaskIds,
+      [destination.droppableId]: destinationTaskIds,
+    })
+
+    const prev = tasks[sourceTaskIds[destination.index - 1]]?.position
+    const next = tasks[sourceTaskIds[destination.index + 1]]?.position
+    const newPosition = getPosition(prev, next)
+
+    await updateTaskPosition({
+      id: movedTaskId,
+      position: newPosition,
+      columnId: destination.droppableId,
     })
   }
 
