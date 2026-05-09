9c4b85b1f59927260fd5bf88045ec3bba30e9308
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-09 14:28:48 +0530

    get board data from db in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import { db } from '@/db'
+import { columnTable, taskTable } from '@/db/schema'
+
 import Board from '@/components/board'
 
-export default function Home() {
+export default async function Home() {
+  const [columns, tasks] = await Promise.all([
+    db.select().from(columnTable).orderBy(columnTable.position),
+    db.select().from(taskTable).orderBy(taskTable.position),
+  ])
+
   return (
     <main className='flex-1 flex'>
       <div className='flex-1 flex flex-col container p-4 w-full mx-auto gap-4 space-y-4'>
