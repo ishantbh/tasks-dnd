d9eafd8712d1ad9418a7bce8165ae9d69fad13e9
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-09 14:22:19 +0530

    update db singleton to use envConfig to access env vars

--- db/index.ts
+++ db/index.ts
@@ -1,14 +1,15 @@
+import '@/envConfig'
 import { drizzle } from 'drizzle-orm/postgres-js'
 
 import * as schema from './schema'
 
-declare global {
-  var db: ReturnType<typeof drizzle> | undefined
+const globalForDb = globalThis as unknown as {
+  db: ReturnType<typeof drizzle> | undefined
 }
 
 export const db =
-  globalThis.db ?? drizzle(process.env.DATABASE_URL!, { schema })
+  globalForDb.db ?? drizzle(process.env.DATABASE_URL!, { schema })
 
 if (process.env.NODE_ENV !== 'production') {
-  globalThis.db = db
+  globalForDb.db = db
 }
