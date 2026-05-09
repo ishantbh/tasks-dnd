a0d05c8419e5ca7ed5510926d50a480993f675a3
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-09 14:29:44 +0530

    update page to normalize db data

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { db } from '@/db'
 import { columnTable, taskTable } from '@/db/schema'
+import { normalizeBoardData } from '@/lib/utils'
 
 import Board from '@/components/board'
 
@@ -9,11 +10,13 @@ export default async function Home() {
     db.select().from(taskTable).orderBy(taskTable.position),
   ])
 
+  const initialData = normalizeBoardData({ columns, tasks })
+
   return (
     <main className='flex-1 flex'>
       <div className='flex-1 flex flex-col container p-4 w-full mx-auto gap-4 space-y-4'>
         <h1 className='text-xl sm:text-2xl font-semibold'>Kanban App</h1>
-        <Board />
+        <Board initialData={initialData} />
       </div>
     </main>
   )
