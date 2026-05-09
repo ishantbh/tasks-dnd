658d60c6daab640d0d0e3b4bcff0c3a844cd58ef
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-09 16:45:30 +0530

    use board store context to pass initial data to board component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { db } from '@/db'
 import { columnTable, taskTable } from '@/db/schema'
 import { normalizeBoardData } from '@/lib/utils'
+import { BoardStoreProvider } from '@/providers/board-store-provider'
 
 import Board from '@/components/board'
 
@@ -16,7 +17,9 @@ export default async function Home() {
     <main className='flex-1 flex'>
       <div className='flex-1 flex flex-col container p-4 w-full mx-auto gap-4 space-y-4'>
         <h1 className='text-xl sm:text-2xl font-semibold'>Kanban App</h1>
-        <Board initialData={initialData} />
+        <BoardStoreProvider initialData={initialData}>
+          <Board />
+        </BoardStoreProvider>
       </div>
     </main>
   )
