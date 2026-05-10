b5f8336c9fa9c4409d2ca8d88f1ec292dd7144fe
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-10 09:01:34 +0530

    update drizzle instance to use schema in type to allow queries

--- db/index.ts
+++ db/index.ts
@@ -4,7 +4,7 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import * as schema from './schema'
 
 const globalForDb = globalThis as unknown as {
-  db: ReturnType<typeof drizzle> | undefined
+  db: ReturnType<typeof drizzle<typeof schema>> | undefined
 }
 
 export const db =
