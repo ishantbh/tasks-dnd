046c1a55f33e9940eaac582795c0e65997efbed0
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-10 09:01:52 +0530

    update seed to include boards

--- db/seed.ts
+++ db/seed.ts
@@ -1,12 +1,28 @@
 import { db } from '@/db'
-import { columnTable, NewColumn, NewTask, taskTable } from '@/db/schema'
+import type { NewBoard, NewColumn, NewTask } from '@/db/schema'
+import { boardTable, columnTable, taskTable } from '@/db/schema'
 
 async function main() {
   try {
+    const dummyBoards: NewBoard[] = [
+      { title: 'My Kanban Board' },
+      { title: 'Another Board' },
+      { title: 'Yet Another Board' },
+    ]
+
+    const insertedBoards = await db
+      .insert(boardTable)
+      .values(dummyBoards)
+      .returning()
+
     const dummyColumns: NewColumn[] = [
-      { title: 'To Do', position: 'a0' },
-      { title: 'In Progress', position: 'a1' },
-      { title: 'Done', position: 'a2' },
+      { title: 'To Do', position: 'a0', boardId: insertedBoards[0].id },
+      { title: 'In Progress', position: 'a1', boardId: insertedBoards[0].id },
+      { title: 'Done', position: 'a2', boardId: insertedBoards[0].id },
+
+      { title: 'Backlog', position: 'a0', boardId: insertedBoards[1].id },
+      { title: 'Doing', position: 'a1', boardId: insertedBoards[1].id },
+      { title: 'Finished', position: 'a2', boardId: insertedBoards[1].id },
     ]
 
     const insertedColumns = await db
