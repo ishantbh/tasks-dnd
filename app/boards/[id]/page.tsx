2b62b38cf04655a9c6fe98b37b6902911c2975ae
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-10 09:05:55 +0530

    update board page to fetch board data and display board component

--- app/boards/[id]/page.tsx
+++ app/boards/[id]/page.tsx
@@ -1,3 +1,12 @@
+import { eq } from 'drizzle-orm'
+
+import { db } from '@/db'
+import { columnTable, taskTable } from '@/db/schema'
+import { normalizeBoardData } from '@/lib/utils'
+import { BoardStoreProvider } from '@/providers/board-store-provider'
+
+import Board from '@/components/board'
+
 export default async function BoardPage({
   params,
 }: {
@@ -5,5 +14,21 @@ export default async function BoardPage({
 }) {
   const { id: boardId } = await params
 
-  return <div>Board Page with id {boardId}</div>
+  const columnsWithTasks = await db.query.columnTable.findMany({
+    where: eq(columnTable.boardId, boardId),
+    orderBy: columnTable.position,
+    with: {
+      tasks: {
+        orderBy: taskTable.position,
+      },
+    },
+  })
+
+  const initialData = normalizeBoardData(columnsWithTasks)
+
+  return (
+    <BoardStoreProvider initialData={initialData}>
+      <Board />
+    </BoardStoreProvider>
+  )
 }
