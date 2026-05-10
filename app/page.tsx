9da89bd39c99cd4b8c8e389d6e60a6c6ad7992ec
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-10 09:02:25 +0530

    update home page to display grid of boards

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,28 @@
-import { db } from '@/db'
-import { columnTable, taskTable } from '@/db/schema'
-import { normalizeBoardData } from '@/lib/utils'
-import { BoardStoreProvider } from '@/providers/board-store-provider'
+import Link from 'next/link'
 
-import Board from '@/components/board'
+import { desc } from 'drizzle-orm'
+
+import { db } from '@/db'
+import { boardTable } from '@/db/schema'
 
 export default async function Home() {
-  const [columns, tasks] = await Promise.all([
-    db.select().from(columnTable).orderBy(columnTable.position),
-    db.select().from(taskTable).orderBy(taskTable.position),
-  ])
-
-  const initialData = normalizeBoardData({ columns, tasks })
+  const boards = await db
+    .select()
+    .from(boardTable)
+    .orderBy(desc(boardTable.createdAt))
 
   return (
-    <main className='flex-1 flex'>
-      <div className='flex-1 flex flex-col container p-4 w-full mx-auto gap-4 space-y-4'>
-        <h1 className='text-xl sm:text-2xl font-semibold'>Kanban App</h1>
-        <BoardStoreProvider initialData={initialData}>
-          <Board />
-        </BoardStoreProvider>
-      </div>
-    </main>
+    <ul className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+      {boards.map((board) => (
+        <li key={board.id}>
+          <Link
+            href={`/boards/${board.id}`}
+            className='block border border-foreground/15 rounded-lg p-4 hover:bg-foreground/5'
+          >
+            {board.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
   )
 }
