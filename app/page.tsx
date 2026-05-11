232eaddaab17f1dae33cc186f2125c73d27e9a28
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-11 20:44:12 +0530

    display create-board modal trigger on home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { desc } from 'drizzle-orm'
 import { db } from '@/db'
 import { boardTable } from '@/db/schema'
 
+import CreateBoard from '@/components/create-board'
+
 export default async function Home() {
   const boards = await db
     .select()
@@ -12,17 +14,22 @@ export default async function Home() {
     .orderBy(desc(boardTable.createdAt))
 
   return (
-    <ul className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-      {boards.map((board) => (
-        <li key={board.id}>
-          <Link
-            href={`/boards/${board.id}`}
-            className='block border border-foreground/15 rounded-lg p-4 hover:bg-foreground/5'
-          >
-            {board.title}
-          </Link>
+    <>
+      <ul className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        {boards.map((board) => (
+          <li key={board.id}>
+            <Link
+              href={`/boards/${board.id}`}
+              className='block border border-foreground/15 rounded-lg p-4 hover:bg-foreground/5'
+            >
+              {board.title}
+            </Link>
+          </li>
+        ))}
+        <li>
+          <CreateBoard />
         </li>
-      ))}
-    </ul>
+      </ul>
+    </>
   )
 }
